9695f66f0e6fb14bb38003d28b50ce1331d495b5
Gabriel Melillo <gabriel.melillo@olx.com>
2018-07-03 12:28:55 +0200

    Fix con IstioConnectionPool

--- lib/kube_objs/istio_connectionpool.py
+++ lib/kube_objs/istio_connectionpool.py
@@ -6,19 +6,23 @@ from __future__ import print_function
 from __future__ import unicode_literals
 
 from kube_obj import KubeSubObj
-from kube_types import Nullable, Integer
+from kube_types import Nullable, Integer, String
 
 
 class IstioConnectionPool(KubeSubObj):
     _defaults = {
         'maxConnections': None,
+        'connectTimeout': None,
     }
 
     _types = {
         'maxConnections': Nullable(Integer),
+        'connectTimeout': Nullable(String)
     }
 
     def render(self):
-        return None if self._data['maxConnections'] is None else {
-                'tcp': {'maxConnections': self._data['maxConnections']}
-            }
+        filtered = {k: v for k, v in self._data.iteritems() if v is not None}
+        self._data = {
+            'tcp': filtered
+        }
+        return self.renderer()
