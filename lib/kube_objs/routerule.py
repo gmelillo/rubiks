141b0b191b66a7d9c8b3c674f9756c5f3a97fd41
William Sandin <william.sandin@olx.com>
2018-07-03 13:42:49 +0200

    Added kube object for istio RouteRule

---
+++ lib/kube_objs/routerule.py
@@ -0,0 +1,44 @@
+# (c) Copyright 2017-2018 OLX
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+from __future__ import unicode_literals
+
+from collections import OrderedDict
+from kube_obj import KubeObj, KubeSubObj
+from kube_types import *
+
+
+class RouteRule(KubeObj):
+    apiVersion = 'config.istio.io/v1alpha2'
+    kind = 'RouteRule'
+    kubectltype = 'istiorouterule'
+
+    _uses_namespace = False
+    _output_order = 200
+
+    _defaults = {
+         'destination': {},
+         'precedence': 1,
+         'version': 'v1',
+        }
+
+    _types = {
+        'destination': Map(String, String),
+        'precedence': Integer,
+        'version': String,
+        }
+
+    def render(self):
+        ret = self.renderer(return_none=True)
+        spec = OrderedDict()
+        if 'destination' in ret:
+            spec['destination'] = ret['destination']
+        if 'precedence' in ret:
+            spec['precedence'] = ret['precedence']
+        spec['route'] = [{'labels': {'version': ret['version']}}]
+
+        return {'metadata': {'name': ret['name']},
+                'spec': spec,
+        }
