8d76e1a3def52a0674d34e1ef92cf8a3c2839c6d
Gabriel Melillo <gabriel.melillo@olx.com>
2018-07-08 14:12:18 +0200

    Added table of content

--- lib/commands/docgen.py
+++ lib/commands/docgen.py
@@ -13,16 +13,28 @@ import os
 class Command_docgen(Command, CommandRepositoryBase):
     """Generate a markdown file with basic description for all object inside rubiks"""
 
+    _description = """
+    This document is automatically generated using the command `docgen` and describe all the object and types that can be used inside Rubiks to configure your cluster
+    """
+
     def populate_args(self, parser):
         pass
 
     def run(self, args):
         objs = load_python.PythonBaseFile.get_kube_objs()
         r = self.get_repository()
-        md = ''
+        doc = '\n'.join([line.strip() for line in self._description.split('\n')])
 
+        md = ''
+        header = '# Rubiks Object Index\n{}\n\n'.format(doc)
+        header += '# Table of contents\n\n'
+
+        header += '- [Objects](#objects)\n'
         for oname in objs.keys():
+            header += '  - [{}](#{})\n'.format(oname, oname.lower())
             md += objs[oname].get_help().render_markdown()
+        header += '\n# Objects\n\n'
 
         with open(os.path.join(r.basepath, 'docs/rubiks.class.md'), 'w') as f:
+            f.write(header)
             f.write(md)
